f6554e759fb4f60fa9bbdc72290f8e91b940507f
Tony(Qihuan) Yang <qihuan.yang@justeattakeaway.com>
2025-02-19 22:48:49 -0700

    Impl: java lsp for nvChad

--- starter/ftplugin/java.lua
+++ starter/ftplugin/java.lua
@@ -31,23 +31,10 @@ function nnoremap(rhs, lhs, bufopts, desc)
 end
 
 local on_attach = function(client, bufnr)
+  local nvlsp = require "nvchad.configs.lspconfig"
+  nvlsp.on_attach(client, bufnr);
+
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  nnoremap('gD', vim.lsp.buf.declaration, bufopts, "Go to declaration")
-  nnoremap('gd', vim.lsp.buf.definition, bufopts, "Go to definition")
-  nnoremap('gi', vim.lsp.buf.implementation, bufopts, "Go to implementation")
-  nnoremap('K', vim.lsp.buf.hover, bufopts, "Hover text")
-  nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts, "Show signature")
-  nnoremap('<space>wa', vim.lsp.buf.add_workspace_folder, bufopts, "Add workspace folder")
-  nnoremap('<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts, "Remove workspace folder")
-  nnoremap('<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts, "List workspace folders")
-  nnoremap('<space>D', vim.lsp.buf.type_definition, bufopts, "Go to type definition")
-  nnoremap('<space>cn', vim.lsp.buf.rename, bufopts, "Rename")
-  nnoremap('<space>ca', vim.lsp.buf.code_action, bufopts, "Code actions")
-  vim.keymap.set('v', "<space>ca", "<ESC><CMD>lua vim.lsp.buf.range_code_action()<CR>",
-    { noremap = true, silent = true, buffer = bufnr, desc = "Code actions" })
-  nnoremap('<space>f', function() vim.lsp.buf.format { async = true } end, bufopts, "Format file")
   nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
   nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
   nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
@@ -58,16 +45,9 @@ local on_attach = function(client, bufnr)
   jdtls.setup_dap({ hotcodereplace = 'auto' })
   jdtls.setup.add_commands()
 
-  -- Default keymaps
-  -- require("lsp.defaults").on_attach(client, bufnr)
-
   -- Java extensions
-  nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
-  nnoremap("<leader>vc", jdtls.test_class, bufopts, "Test class (DAP)")
-  nnoremap("<leader>vm", jdtls.test_nearest_method, bufopts, "Test method (DAP)")
-  nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
-  nnoremap("<space>ec", jdtls.extract_constant, bufopts, "Extract constant")
-  nnoremap("<space>em", [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]], bufopts, "Extract method")
+  nnoremap("<leader>tc", jdtls.test_class, bufopts, "Test class (DAP)")
+  nnoremap("<leader>tm", jdtls.test_nearest_method, bufopts, "Test method (DAP)")
 end
 
 
--- starter/lua/configs/conform.lua
+++ starter/lua/configs/conform.lua
@@ -7,11 +7,11 @@ local options = {
     typescript = { "prettierd", "prettier", stop_after_first = true },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
 
 return options
