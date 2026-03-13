e7cae5360428b68bb521e79f558123a2ea77114e
Tony(Qihuan) Yang <qihuan.yang@justeattakeaway.com>
2026-03-13 12:00:50 -0600

    Add lazygit config and nvim keybinding (Space+gg)

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- LazyGit
+map("n", "<leader>gg", function()
+  require("nvchad.term").toggle { pos = "float", id = "lazygit", cmd = "lazygit" }
+end, { desc = "LazyGit" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
